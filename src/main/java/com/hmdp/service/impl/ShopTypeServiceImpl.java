213d7ad35e040658b802d6a03f01c732c459b4e0
myt <miaoyt0238@gmail.com>
2026-05-11 18:39:09 +0800

    5/11 关于商品和商品种类的缓存方法，以及之前未提交的拦截器方面的内容。

--- src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
@@ -1,10 +1,19 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.ShopType;
 import com.hmdp.mapper.ShopTypeMapper;
 import com.hmdp.service.IShopTypeService;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+import static com.hmdp.utils.RedisConstants.CACHE_SHOP_TYPE_KEY;
 
 /**
  * <p>
@@ -17,4 +26,26 @@ import org.springframework.stereotype.Service;
 @Service
 public class ShopTypeServiceImpl extends ServiceImpl<ShopTypeMapper, ShopType> implements IShopTypeService {
 
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public Result queryTypeList() {
+        String key = CACHE_SHOP_TYPE_KEY + "list";
+        String typeJson = stringRedisTemplate.opsForValue().get(key);
+        if (StrUtil.isNotBlank(typeJson)) {
+            List<ShopType> typeList = JSONUtil.toList(JSONUtil.parseArray(typeJson), ShopType.class);
+            if (typeList.isEmpty()) {
+                return Result.fail("未查询到商品种类");
+            }
+            return Result.ok(typeList);
+        }
+
+        List<ShopType> typeList = query().orderByAsc("sort").list();
+        stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(typeList));
+        if (typeList.isEmpty()) {
+            return Result.fail("未查询到商品种类");
+        }
+        return Result.ok(typeList);
+    }
 }
